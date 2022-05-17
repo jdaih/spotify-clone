a920901fa321979deea65336664e73443fa025b1
jdaih <justinzero2008@yahoo.com>
2022-05-17 15:19:31 -0500

    added comments

--- src/components/loginAuthorization.js
+++ src/components/loginAuthorization.js
@@ -1,9 +1,8 @@
+// This file sets up URL to authorize app
+
 export const authEndpoint = 'https://accounts.spotify.com/authorize'
-
 const redirectUri = 'http://localhost:3000/'
-
 const clientId = '8cc42665e8534e76b3c186d4d080eea6'
-
 
 //Scopes specify what app is allowed to do
 const scopes = [
