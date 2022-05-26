d3b5109c60d1506477653853cede26ff0bfe18d3
jdaih <justinzero2008@yahoo.com>
2022-05-26 17:18:12 -0500

    changed more barney info

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -13,9 +13,9 @@ const Body = ({ spotify }) => {
             <div className='body_content'>
                 <img src={discover_weekly?.images[0].url} alt='Album Cover'/>
             <div className='body_text'>
-                <strong>ALBUM</strong>
-                <h2> Start Singing with Barney </h2>
-                <p> Barney * 2003 * 23 songs, 35 min 30 sec </p>
+                <strong>PLAYLIST</strong>
+                <h2> Discover Weekly </h2>
+                <p> Your weekly mixtape of fresh music. Enjoy new music and deep cuts picked for you. Updates every Monday. </p>
             </div>
         </div>
 
