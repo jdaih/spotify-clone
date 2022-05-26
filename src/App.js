4acfa3691f9cd559510ca0a69855582d75e38b7d
jdaih <justinzero2008@yahoo.com>
2022-05-26 08:15:52 -0500

    more authorization code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import Login from './components/Login.js'
+import Login from './login/Login.js'
+import { getTokenFromUrl } from './login/loginAuthorization'
+import SpotifyWebApi from 'spotify-web-api-js'
+import Dashboard from './components/Dashboard'
+import { useDataLayerValue } from './DataLayer'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then((user) => {
+        console.log(user)
+
+        dispatch({
+          type: 'Set_USER',
+          user: user
+        })
+      });
+    }
+  }, []);
+
   return (
-    <div className="app">
-      <Login />
+    <div className="App">
+      {token ? <Dashboard /> : (<Login />)}
     </div>
   );
 }
--- src/login/loginAuthorization.js
+++ src/login/loginAuthorization.js
@@ -1,4 +1,5 @@
 // This file sets up URL to authorize app
+// https://developer.spotify.com/documentation/web-api/quick-start/
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize'
 const redirectUri = 'http://localhost:3000/'
@@ -12,5 +13,17 @@ const scopes = [
     'user-top-read',
     'user-modify-playback-state',
 ]
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split('&')
+        .reduce((initial, item) => {
+            let parts = item.split('=')
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+}
+
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`
