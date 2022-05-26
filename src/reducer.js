75936678763acf0eaa1e489ac63064cbb0556db9
jdaih <justinzero2008@yahoo.com>
2022-05-26 16:59:19 -0500

    removed api token

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,6 @@ export const initialState = {
     playlist: [],
     playing: false,
     item: null,
-    // token: 'BQDeGhcJiqe0NXFvHJsn7WRYCXH_M5_SuUefNN1tGax4SP9cdVGSuVOFEkWPxCAEedvKvCM6a730ev2YKU7puA3U1C6Z_XXCiYdWxdxQUurelpRpew8aIFpLyqp-UCRnCVGWi1lIvopbaqnUC2AMm3-qBViZ',
-}
-        //authenticated, change to null when done
 
 const reducer = (state, action) => {
     switch(action.type) {
