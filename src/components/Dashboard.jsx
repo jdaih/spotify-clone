57d5ca72743220d4d2a80492bd44993ac526398a
jdaih <justinzero2008@yahoo.com>
2022-05-26 08:20:55 -0500

    added components

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+import Header from './Header'
+
+const Dashboard = () => {
+    return (
+        <div>
+    
+        </div>
+  )
+}
+
+export default Dashboard
